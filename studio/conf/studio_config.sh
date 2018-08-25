5a010cbc9f0a63645a82dcec31a964af0b8c19cd
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-08-25 11:38:26 -0400

    CAHNGES IN STUDIO START/STOP SCRIPTS AND ALSO UPDATE THE CONF file for studio

--- studio/conf/studio_config.sh
+++ studio/conf/studio_config.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+
+export JAVA_HOME=/app/localstorage/jdk1.8.0_171
+export PATH=$PATH:$JAVA_HOME/bin
+export HOST_NAME=`hostname`
+export STUDIO_CONFIG=/app/localstorage/DSE/datastax-studio/conf/configuration.yaml
+export WORK_DIR=\\/app\\/localstorage\\/DSE\\/studio_user_data
+
+
+mv /app/localstorage/DSE/datastax-studio-6.0.0 /app/localstorage/DSE/datastax-studio
+sed -ri 's/( )?(  httpBindAddress:).*/\2 '"$HOST_NAME"'/' "$STUDIO_CONFIG"
+mkdir -p /app/localstorage/DSE/tmp
+mkdir -p $WORK_DIR
+ser -ri 's/( )?(  baseDirectory:).*/\2 '"$WORK_DIR"'/' "$STUDIO_CONFIG"
+
+
+
+
