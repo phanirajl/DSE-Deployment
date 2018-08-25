531947dfa89834a17ac80e4a2a5ce688902bd232
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-08-25 11:21:46 -0400

    agent config file update

--- agent/bin/agent_start.sh
+++ agent/bin/agent_start.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+export DATASTAX_AGENT_HOME=/app/localstorage/DSE/datastax-agent
+export JAVA_HOME=/app/localstorage/jdk1.8.0_171
+export PATH=$PATH:$JAVA_HOME/bin
+$DATASTAX_AGENT_HOME/bin/datastax-agent -f &
--- agent/bin/agent_stop.sh
+++ agent/bin/agent_stop.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+export DATASTAX_AGENT_HOME=/app/localstorage/DSE/datastax-agent
+export JAVA_HOME=/app/localstorage/jdk1.8.0_171
+export PATH=$PATH:$JAVA_HOME/bin
+
+pid=`pgrep -f datastax-agent`
+kill -9 $pid
