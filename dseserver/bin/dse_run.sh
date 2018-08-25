f2968edb5b6aa0e7b4a1d6ab921fce74e85a9ad4
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-08-25 10:06:31 -0400

    dse start/stop script update

--- dseserver/bin/dse_run.sh
+++ dseserver/bin/dse_run.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+export DSE_HOME=/app/localstorage/DSE/dse
+export cassandra_dir=/app/localstorage/DSE/var/lib/cassandra
+export CASSANDRA_CONFIG=/app/localstorage/DSE/dse/resources/cassandra/conf/cassandra.yaml
+export DSE_CONFIG=/app/localstorage/DSE/dse/resources/dse/conf/dse.yaml
+export CASSANDRA_LOG_DIR=/app/localstorage/DSE/var/log/cassandra
+export SPARK_WORKER_DIR="/app/localstorage/DSE/var/lib/spark/worker"
+export SPARK_EXECUTOR_DIRS="/app/localstorage/DSE/var/lib/spark/rdd"
+export SPARK_WORKER_LOG_DIR="/app/localstorage/DSE/var/log/spark/worker"
+export SPARK_MASTER_LOG_DIR="/app/localstorage/DSE/var/log/spark/master"
+export ALWAYSON_SQL_LOG_DIR="/app/localstorage/DSE/var/log/spark/alwayson_sql"
+export JAVA_HOME=/app/localstorage/jdk1.8.0_171
+export PATH=$PATH:$JAVA_HOME/bin
+
+$DSE_HOME/bin/dse cassandra -Djna.tmpdir=/app/localstorage/DSE/tmp -k -f &
