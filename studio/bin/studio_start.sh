7e80f66f40afc5f8be4868aa467e9c14b37b0155
rahulgaitonde1989 <gaitonderahul@gmail.com>
2018-08-25 11:38:59 -0400

    CAHNGES IN STUDIO START/STOP SCRIPTS AND ALSO UPDATE THE CONF file for studio

--- studio/bin/studio_start.sh
+++ studio/bin/studio_start.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+export STUDIO_JVM_ARGS="-Xmx4g -Djava.io.tmpdir=/app/localstorage/DSE/tmp"
+export JAVA_HOME=/app/localstorage/jdk1.8.0_171
+export PATH=$PATH:$JAVA_HOME/bin
+export DSE_STUDIO_HOME=/app/localstorage/DSE/datastax-studio
+
+$DSE_STUDIO_HOME/bin/server.sh &
+
--- studio/bin/studio_stop.sh
+++ studio/bin/studio_stop.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+export STUDIO_JVM_ARGS="-Xmx4g -Djava.io.tmpdir=/app/localstorage/DSE/tmp"
+export JAVA_HOME=/app/localstorage/jdk1.8.0_171
+export PATH=$PATH:$JAVA_HOME/bin
+export DSE_STUDIO_HOME=/app/localstorage/DSE/datastax-studio
+
+$DSE_STUDIO_HOME/bin/server.sh &
+
+pid=`pgrep -f studio-server*`
+kill -9 $pid
+
